b4bf340134ad2034e6593825275ac014afd72c58
sleahy115 <sleahy115@gmail.com>
2017-10-31 00:25:04 -0600

    start slideshow

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,14 @@ $(document).ready(function(){
   $(".hamburger").click(function(){
     $(".links").toggle();
   });
+  $("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('.slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('.slideshow');
+	}, 3000);
  });
